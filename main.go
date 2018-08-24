0842985b84768c2fa32d8d99c4de43028e1c36f1
CornierKhan1 <jordyalexlyall@gmail.com>
2018-08-24 15:56:10 +1000

    fixy wixy

--- main.go
+++ main.go
@@ -1,32 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"encoding/xml"
-)
-
-const (
-	// Header is a generic XML header suitable for use with the output of Marshal.
-	// This is not automatically added to any output of this package,
-	// it is provided as a convenience.
-	Header = `<?xml version="1.0" encoding="UTF-8"?>` + "\n"
 )
 
 // The Check struct(ure) will attempt to retrieve all the namespace data.
-// Assuming that namespaces that don't exist are given a "nil", the first result that
-// isn't a "nil" will be used as the template response.
+// Assuming that namespaces that don't exist are given a "nil", the first result that  isn't a "nil" will be used as the template response.
 
 type Check struct {
 	// ECommerce Namespaces
-	CDS xml.Name `ecs:"CheckDeviceStatus"`
-	LET	xml.Name `ecs:"ListETickets"`
-	NETS xml.Name `ecs:"NotifyETicketsSynced"`
-	PT xml.Name	`ecs:"PurchaseTitle"`
+	CDS  string `ecs:"CheckDeviceStatus"`
+	LET  string `ecs:"ListETickets"`
+	NETS string `ecs:"NotifyETicketsSynced"`
+	PT   string `ecs:"PurchaseTitle"`
 
 	// Identity Authentication Namespaces
+	CR  string `ecs:"CheckRegistration"`
+	GRI string `ecs:"GetRegistrationInfo"`
+	REG string `ecs:"Register"`
+	UNR string `ecs:"Unregister"`
 }
+
 func main() {
 
+	// ChRes is a variable that's in the form of JSON. This organises all the data into sub-variables like ChRes.CDS.
+	// This is probably my favourite thing in GoLang to be honest.
+	ChRes := Check{
+		CDS:  "",
+		LET:  "",
+		NETS: "",
+		PT:   "",
+		CR:   "",
+		GRI:  "",
+		REG:  "",
+		UNR:  "",
+	}
+	fmt.Println(ChRes)
 
+	// http.ListenAndServe starts a HTTP server, which is important to take note of as we will be using this to deliver the SOAP.
 	http.ListenAndServe(":80", nil)
 }
