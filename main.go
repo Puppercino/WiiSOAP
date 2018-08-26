958a3605f119b1f17c819cc8f9b159ca81d69ac3
CornierKhan1 <jordyalexlyall@gmail.com>
2018-08-26 17:00:38 +1000

    not working, but its something

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -23,9 +25,13 @@ type Check struct {
 }
 
 func main() {
-
+	file, err := ioutil.ReadFile("SOAP-WSC/ECS/checkDeviceStatus.xml")
 	// ChRes is a variable that's in the form of JSON. This organises all the data into sub-variables like ChRes.CDS.
 	// This is probably my favourite thing in GoLang to be honest.
+	if err != nil {
+		panic(err)
+		return
+	}
 	ChRes := Check{
 		CDS:  "",
 		LET:  "",
@@ -35,6 +41,11 @@ func main() {
 		GRI:  "",
 		REG:  "",
 		UNR:  "",
+	}
+	err = xml.Unmarshal(file, &ChRes)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
 	}
 	fmt.Println(ChRes)
 
